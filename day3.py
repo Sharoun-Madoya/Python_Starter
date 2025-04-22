6bcd482b527d96c3aa41a3fd23a3c0ad1c760b63
Sharoun <sharounmadoya@gmail.com>
2025-04-22 09:02:34 +0300

    :hammer: updated day3 lists

---
+++ day3.py
@@ -0,0 +1,47 @@
+#LIST
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+first_fruit = fruits[-4]
+last_fruit = fruits[-1]
+second_last = fruits[-2]
+print(first_fruit)      # banana
+print(last_fruit)       # lemon
+print(second_last)      # mango
+
+
+
+fruits[0] = 'avocado'
+print(fruits)       #  ['avocado', 'orange', 'mango', 'lemon']
+fruits[1] = 'apple'
+print(fruits)       #  ['avocado', 'apple', 'mango', 'lemon']
+last_index = len(fruits) - 1
+fruits[last_index] = 'lime'
+print(fruits)        #  ['avocado', 'apple', 'mango', 'lime']
+
+
+
+does_exist = 'banana' in fruits
+print(does_exist)  # True
+does_exist = 'lime' in fruits
+print(does_exist)  # False
+
+
+
+fruits.append('apple')
+print(fruits)           # ['banana', 'orange', 'mango', 'lemon', 'apple']
+fruits.append('lime')   # ['banana', 'orange', 'mango', 'lemon', 'apple', 'lime']
+print(fruits)
+
+
+fruits.sort()
+print(fruits)             # sorted in alphabetical order, ['banana', 'lemon', 'mango', 'orange']
+fruits.sort(reverse=True)
+print(fruits) # ['orange', 'mango', 'lemon', 'banana']
+
+#There are several ways to join, or concatenate, two or more lists in Python
+
+#Joining using extend() method The extend() method allows to append list in a list. See the example below.
+# syntax
+list1 = ['item1', 'item2']
+list2 = ['item3', 'item4', 'item5']
+list1.extend(list2)
---
+++ day4.py
@@ -0,0 +1,12 @@
+# person_info = {
+#    'firstname':'Asabeneh',
+#    'lastname':'Yetayeh',
+#    'country':'Finland',
+#    'city':'Helsinki'
+#    }
+
+# print(person_info[0])
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+
+print(fruits[0])
